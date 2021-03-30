7ba382bc4d4c89f48f0b108f7f6752dbb8a39959
Vusal Akbarli <vusalakberli@gmail.com>
2021-03-30 16:10:49 +0400

    index.html-de user panelde kicik deyisiklik

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -32,7 +32,11 @@ return true;
 }
 window.onerror = CC_noErrors;
 
-
+jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
+});
 
 /*
 $(document).ready(function(){
